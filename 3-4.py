fc44ed3b3c089ccd4f38204c192ff3caa2c1cee4
Vladiko <v.sodolevskiy@gmail.com>
2021-10-11 17:01:02 +0300

    3.3 created, half done

---
+++ 3-4.py
@@ -0,0 +1,13 @@
+#create random string with lower and upper case and with numbers
+str_var = "asdjhJVYGHV42315gvghvHGV214HVhjjJK"
+
+#find first symbol of string
+first_symbol = str_var[0]
+print(first_symbol)
+
+#find the last symbol of string
+last_symbol = str_var[-1]
+print(last_symbol)
+
+#slice first 8 symbols
+print(str_var[slice(8)])
