2a498b9e5d36053750c27f9953f01794f25cf90f
Vlad <vladiko@VladikoKali.Vladiko_Kali_Dom>
2021-10-13 20:04:46 +0300

    3.7 done

--- 3-7/3-7.py
+++ 3-7/3-7.py
@@ -1,3 +1,27 @@
 from string import ascii_letters, ascii_lowercase, digits, ascii_lowercase, ascii_uppercase
-set1 = set(ascii_uppercase + ascii_letters + ascii_lowercase + digits[3:7])
-print(set1)
+
+#generate 2 sets with not unique numbers
+set1 = set(ascii_uppercase + ascii_letters + ascii_lowercase + digits)
+set2 = set(ascii_uppercase + ascii_letters + ascii_lowercase + digits)
+print("SET1:", set1)
+
+#create tuple from intersection of first and second set
+tpl_intersec = tuple(set1.intersection(set2))
+print("tuple  intersection:", tpl_intersec)
+
+#create tuple from difference first and second set
+tpl_difference = tuple(set1.difference(set2)) 
+print("tuple difference:", set2)
+
+#slice first 3 symbols from 1st tuple
+slc_three_symb = tpl_intersec[:3]
+print("slice first three symbols:", slc_three_symb)
+
+#print only symbols with numbers from second set
+print("only digits:", set(digits).intersection(set2))
+
+#reverse tuple using slice
+print("reverse:", tpl_intersec[::-1])
+
+#convert both tuples to list
+print(''.join(tpl_intersec))
